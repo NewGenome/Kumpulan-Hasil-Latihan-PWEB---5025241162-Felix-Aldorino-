927d736bb310c3f12c358096b75ce21210811e71
NewGenome <crisisgear0@gmail.com>
2025-12-01 07:10:16 +0700

    Add files via upload

---
+++ Pertemuan 14/cek_login.php
@@ -0,0 +1,114 @@
+
+2
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
+13
+14
+15
+16
+17
+18
+19
+20
+21
+22
+23
+24
+25
+26
+27
+28
+29
+30
+31
+32
+33
+34
+35
+36
+37
+38
+39
+40
+41
+42
+43
+44
+45
+46
+47
+48
+49
+50
+51
+52
+53
+54
+55
+56
+57
+<?php 
+// mengaktifkan session pada php
+session_start();
+ 
+// menghubungkan php dengan koneksi database
+include 'koneksi.php';
+ 
+// menangkap data yang dikirim dari form login
+$username = $_POST['username'];
+$password = $_POST['password'];
+ 
+ 
+// menyeleksi data user dengan username dan password yang sesuai
+$login = mysqli_query($koneksi,"select * from user where username='$username' and password='$password'");
+// menghitung jumlah data yang ditemukan
+$cek = mysqli_num_rows($login);
+ 
+// cek apakah username dan password di temukan pada database
+if($cek > 0){
+ 
+	$data = mysqli_fetch_assoc($login);
+ 
+	// cek jika user login sebagai admin
+	if($data['level']=="admin"){
+ 
+		// buat session login dan username
+		$_SESSION['username'] = $username;
+		$_SESSION['level'] = "admin";
+		// alihkan ke halaman dashboard admin
+		header("location:halaman_admin.php");
+ 
+	// cek jika user login sebagai pegawai
+	}else if($data['level']=="pegawai"){
+		// buat session login dan username
+		$_SESSION['username'] = $username;
+		$_SESSION['level'] = "pegawai";
+		// alihkan ke halaman dashboard pegawai
+		header("location:halaman_pegawai.php");
+ 
+	// cek jika user login sebagai pengurus
+	}else if($data['level']=="pengurus"){
+		// buat session login dan username
+		$_SESSION['username'] = $username;
+		$_SESSION['level'] = "pengurus";
+		// alihkan ke halaman dashboard pengurus
+		header("location:halaman_pengurus.php");
+ 
+	}else{
+ 
+		// alihkan ke halaman login kembali
+		header("location:index.php?pesan=gagal");
+	}	
+}else{
+	header("location:index.php?pesan=gagal");
+}
+ 
+?>
